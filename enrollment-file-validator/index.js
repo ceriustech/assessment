67bdbbca5477f02fcffe111c95bf104d966781d6
Bill <ceriustech@gmail.com>
2021-12-28 22:58:45 -0600

    added comments to the enrollment file validator

--- enrollment-file-validator/index.js
+++ enrollment-file-validator/index.js
@@ -6,24 +6,33 @@ const path = require('path');
 	- Sort all of the enrollees in ascending order by last and first name
 	- If there are duplicate user IDs in the file, only the record with the highest version number
 	  will be included
+	- Write the data for each company into it's own respective file
 */
 
 const readFile = (path) => {
+	// Dynamically set file paths
 	fs.readFile(
 		path,
 		{
 			encoding: 'utf8',
 		},
 		(err, data) => {
+			// the lines constant returns an array of string data where each index in the array pertains to a single enrollee's data
 			const lines = data.split('\n');
 
+			// an empty object that will hold the data for each enrollee
 			let enrolleesByCompany = {};
 
 			for (const line of lines) {
 				const enrollee = {};
+
+				// returns multiple arrays where each enrollee record is an array with each csv value is separated as string data within the array
 				const values = line.split(',');
+
+				// destructuring the string data from the [1] index in each array so that it can have it's own key/value pair within the enrollee object
 				const [firstName, lastName] = values[1].split(' ');
 
+				// building the key/value pairs that will make up the enrolee object
 				enrollee['id'] = values[0];
 				enrollee['firstName'] = firstName;
 				enrollee['lastName'] = lastName;
@@ -34,16 +43,20 @@ const readFile = (path) => {
 					enrolleesByCompany[enrollee.company] = {};
 				}
 
+				// the existingEnrollee object pertains to the record for an enrolee with the highest version number that also has a duplicate ID
 				const existingEnrollee =
 					enrolleesByCompany[enrollee.company][enrollee.id];
+
 				if (existingEnrollee && existingEnrollee.version > enrollee.version) {
 					continue;
 				}
 
+				// builds the enrolleesByCompany object with each enrollee object
 				enrolleesByCompany[enrollee.company][enrollee.id] = enrollee;
 			}
 
 			for (const key in enrolleesByCompany) {
+				// transforms the enrolleesByCompany object into an array and sort the first names in ascending order
 				const content = Object.values(enrolleesByCompany[key])
 					.sort((a, b) => {
 						if (a.firstName < b.firstName) return -1;
@@ -54,11 +67,17 @@ const readFile = (path) => {
 					})
 					.reduce((prev, content) => {
 						const { id, firstName, lastName, version, company } = content;
+						console.log(
+							prev + `${id}, ${firstName} ${lastName}, ${version}, ${company}\n`
+						);
+
+						// return the records that will be used as data for each individual file that pertains to a company
 						return (
 							prev + `${id}, ${firstName} ${lastName}, ${version}, ${company}\n`
 						);
 					}, '');
-				console.log(content);
+
+				// this will generate files that pertain to each respective company in the csv text file
 				fs.writeFile(`${key}.txt`, content, (error) => {
 					console.log(error);
 				});
